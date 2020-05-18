9ad97b8a08a81279b5d9ce6cdc36742e4fd8b22c
James Benjamin <james.abenj@gmail.com>
2020-05-18 17:08:23 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
 
   def create
     byebug
-    Student.create(params[:student])
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
     redirect_to students_path
   end
 
